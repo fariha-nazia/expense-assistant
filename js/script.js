4aa893f950d04aad76f2dcd29a33ba2b375490c1
Fariha Nazia <farihanazia@Mac.fritz.box>
2025-09-02 13:01:01 +0200

    we can now see the balance, do the saving calculation in percentage, and see all the value in result.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 // console.log('js is connected');
 
+// Step 1 - catching the calculate btn by ID
+const calculateBtn = document.getElementById("calculate");
+calculateBtn.addEventListener('click', function(){
+    // console.log('click is working');
+
 // Step 2 - declaring all the constants globally
     const incomeField = parseFloat(document.getElementById("income").value);
     const softwareField = parseFloat(document.getElementById("software").value);
@@ -8,28 +13,16 @@
 
     // if you observe you will see i wrote income, software ... inside {}, this means I am trying to show the output in form of object.
     // console.log({incomeField, softwareField, coursesField, internetField});
-    console.table({incomeField, softwareField, coursesField, internetField});
-
-// Step 1 - catching the calculate btn by ID
-const calculateBtn = document.getElementById("calculate");
-calculateBtn.addEventListener('click', function(){
-    // console.log('click is working');
-
-/* // Step 2 - declaring all the constants globally
-    const incomeField = parseFloat(document.getElementById("income").value);
-    const softwareField = parseFloat(document.getElementById("software").value);
-    const coursesField = parseFloat(document.getElementById("courses").value);
-    const internetField = parseFloat(document.getElementById("internet").value); */
-
+    // console.table({incomeField, softwareField, coursesField, internetField});
 
     // Step 3 - doing the calculation of our expenses. 
     const totalExpenses = softwareField + coursesField + internetField;
-    console.log(totalExpenses);
+    // console.log(totalExpenses);
 
     // Step 4 - Finding the balance by subtracting Income from expense
     const remainingBalance = incomeField - totalExpenses;
-    console.log(remainingBalance);
-    console.table({totalExpenses, remainingBalance});
+    // console.log(remainingBalance);
+    // console.table({totalExpenses, remainingBalance});
 
     // Step 5 - Now that we have total expense and remaining balance we will set this value in the result section
     const totalExpensesSetInField = document.getElementById("total-expenses");
@@ -48,9 +41,38 @@ const calculateSavingsBtn = document.getElementById("calculate-savings");
 calculateSavingsBtn.addEventListener("click", function(){
     // console.log('btn is clicked');
 
-    // Step 7 - fetching the savings input field
+    // Step 7 - declaring all the constants globally
+    const incomeField = parseFloat(document.getElementById("income").value);
+    const softwareField = parseFloat(document.getElementById("software").value);
+    const coursesField = parseFloat(document.getElementById("courses").value);
+    const internetField = parseFloat(document.getElementById("internet").value);
+    console.table({incomeField, softwareField, coursesField, internetField});
+
+    // Step 8 - fetching the savings input field
     const savingFieldPercentage = parseFloat(document.getElementById("savings").value);
     console.log(savingFieldPercentage);
 
-    // 
+    // Step 9 - doing the percentage
+    // Step 9.1 - doing the calculation of our expenses. 
+    const totalExpenses = softwareField + coursesField + internetField;
+    console.log(totalExpenses);
+    const remainingBalance = incomeField - totalExpenses;    
+    console.table({totalExpenses, remainingBalance});
+
+    // tep 9.3 - doing the percentage
+    const savingAmount = (savingFieldPercentage * remainingBalance) / 100;
+    console.log(savingAmount);
+
+    const NewBalanceAfterSaving = remainingBalance - savingAmount;
+    console.log(NewBalanceAfterSaving);
+
+    const savingAmountSettingInField = document.getElementById("savings-amount");
+    savingAmountSettingInField.innerText = savingAmount.toFixed(2);
+
+    // Step 10 -
+    const remainingElement = document.getElementById("remaining-balance");
+    remainingElement.innerText = NewBalanceAfterSaving.toFixed(2);
+
+
+
 });
